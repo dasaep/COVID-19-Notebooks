a977527854fe25b9328b1ff79c8fe36781717cea
dasaep <50528219+dasaep@users.noreply.github.com>
2020-04-15 21:15:34 -0400

    Update jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,5 +1,5 @@
 c.JupyterHub.tornado_settings = {
     'headers': {
-        'Content-Security-Policy': "frame-ancestors 'localhost:80'",
+        'Content-Security-Policy': "frame-ancestors '127.0.0.1:80'",
     }
 }
